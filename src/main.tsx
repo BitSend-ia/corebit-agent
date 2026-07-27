36094502710c111972d752e9f7c77ad028f65f15
Fábio Franzoni <127989104+BitSend-ia@users.noreply.github.com>
2026-07-27 00:29:25 -0300

    Change CSS import and implement update checks
    Updated CSS import and added update check functionality.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+import "./index.css";
+import { checkForUpdates } from "./updater";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
+
+// checar atualizacao 5s depois do app subir e a cada 6 horas
+setTimeout(checkForUpdates, 5000);
+setInterval(checkForUpdates, 6 * 60 * 60 * 1000);
